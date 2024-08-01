49492760be1bd9a2c9840c909968a117af4293a2
azamku <aziz.amkouy@gmail.com>
2024-08-01 16:42:44 +0200

    maj du fichier streamlit-app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,4 @@ st.title("API FastAPI avec Streamlit")
 
 text_input = st.text_input("Entrez le texte pour prédiction")
 
-if st.button("Predict"):
-    if text_input:
-        response = requests.post("http://127.0.0.1:8000/predict", json={"text": text_input})
-        if response.status_code == 200:
-            st.write(response.json())
-        else:
-            st.write("Erreur dans la prédiction")
-    else:
-        st.write("Veuillez entrer un texte.")
+
