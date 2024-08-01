e08ab558d8639d67c111cb4613f471c62f44ee7c
azamku <aziz.amkouy@gmail.com>
2024-08-02 00:07:23 +0200

    maj du fichier streamlit-app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ text_input=st.text_input("Entrer le texte pour la prediction de tags")
 if st.button("Predict"):
 
 	response = requests.post("http://localhost:8000/predict/", json={"text": text_input})
+
 	if response.status_code == 200:
 
 		st.write(response.json())
