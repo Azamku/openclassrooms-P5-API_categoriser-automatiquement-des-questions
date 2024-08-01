834df527fa30c9f43fa4bc344cc5f2b015583456
azamku <aziz.amkouy@gmail.com>
2024-08-01 22:48:58 +0200

    maj du fichier streamlit-app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import requests
 
 st.title("API FastAPI avec Streamlit !!!!!")
-text_input=st.text_input("hello")
+text_input=st.text_input("Entrer le texte pour la prediction de tags")
