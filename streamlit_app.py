a945a0933efd10bd57024ca222382004b1a2859b
azamku <aziz.amkouy@gmail.com>
2024-08-02 11:05:11 +0200

    maj du fichier streamlit-app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ import requests
 
 st.title("API FastAPI avec Streamlit !!!!!")
 text_input=st.text_input("Entrer le texte pour la prediction de tags")
+
+
 if st.button("Predict"):
 
-	response = requests.post("http://localhost:8000/predict", json={"text": text_input})
+	response = requests.get("http://localhost:8000/predict", json={"text": text_input})
 
 	if response.status_code == 200:
 
