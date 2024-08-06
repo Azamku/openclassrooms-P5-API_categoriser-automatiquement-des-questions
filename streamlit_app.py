4690895e08ba6d8425a4acaa92e4869e48d4f6a2
azamku <aziz.amkouy@gmail.com>
2024-08-06 23:54:52 +0200

    Maj streamlitapp.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,26 @@
 ﻿import streamlit as st
 import spacy
+import numpy
+import pandas as pd
 
 st.title("Test Spacy Import")
 
-# Charger le modèle spacy
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Hello, world!")
+# Afficher les versions des bibliothèques
+st.write(f"Version de Spacy: {spacy.__version__}")
+st.write(f"Version de Numpy: {numpy.__version__}")
+st.write(f"Version de Pandas: {pd.__version__}")
 
-# Afficher le texte analysé
-st.write(doc.text)
+# Charger le modèle spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+    st.write("Modèle Spacy chargé avec succès.")
+except Exception as e:
+    st.write(f"Erreur lors du chargement du modèle Spacy: {e}")
+
+# Analyser un texte d'exemple
+try:
+    doc = nlp("Hello, world!")
+    st.write("Texte analysé avec succès.")
+    st.write(doc.text)
+except Exception as e:
+    st.write(f"Erreur lors de l'analyse du texte: {e}")
