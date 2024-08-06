99020d80811ee7a6292d14b5f4f63cf6b1b1b083
azamku <aziz.amkouy@gmail.com>
2024-08-07 00:02:05 +0200

    Maj streamlitapp.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,4 @@ st.write(f"Version de Spacy: {spacy.__version__}")
 st.write(f"Version de Numpy: {numpy.__version__}")
 st.write(f"Version de Pandas: {pd.__version__}")
 
-# Charger le modèle spacy
-try:
-    nlp = spacy.load("en_core_web_sm")
-    st.write("Modèle Spacy chargé avec succès.")
-except Exception as e:
-    st.write(f"Erreur lors du chargement du modèle Spacy: {e}")
 
