88cbabb9202c86e7e4339525527ee4451de86347
azamku <aziz.amkouy@gmail.com>
2024-08-07 11:23:49 +0200

    Maj streamlitapp.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,12 @@ except OSError:
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
     st.write("Modèle Spacy chargé avec succès après installation.")
+
+# Analyser un texte d'exemple
+try:
+    doc = nlp("Hello, world!")
+    st.write("Texte analysé avec succès.")
+    # Afficher le texte analysé
+    st.write(doc.text)
+except Exception as e:
+    st.write(f"Erreur lors de l'analyse du texte: {e}")
